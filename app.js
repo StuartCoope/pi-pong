c0c971f6cc6637854f12f11a53f2b0c1f74c8b4c
Stuart <stuart.coope@pay4later.com>
2015-12-04 13:18:18 +0000

    Resolved conflicts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var gpio = require("pi-gpio");
-
+var inputs = require("inputs");
 var ledPin = 7;
+
+var buttonInputs = [11];
 
 var ledOn = function(callback) {
   gpio.open(ledPin, "output", function(err) {
@@ -14,9 +16,26 @@ var ledOff = function(callback) {
   gpio.close(ledPin, callback);
 }
 
-var shutdown = function(){
-  ledOff(process.exit);
+var shutdown = function() {
+	ledOff(process.exit);
 };
+
+var readButton = function(pin, callback) {
+	gpio.open(pin, "input pullup", function(err) {
+	    gpio.read(pin, callback);
+	});
+};
+
+var loop = function(){
+	buttonInputs.forEach(function(id){
+		readButton(id, function(err, value) {
+			console.log(value);
+    	});
+	});
+	setTimeout(loop, 10);
+};
+
+loop();
 
 var repl = require('repl');
 
