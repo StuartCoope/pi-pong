b375fbc922abab3635724a9104c6b802234fd9b3
Aman <aman.ramkumar@pay4later.com>
2015-12-04 12:48:40 +0000

    sedfg

---
+++ app.js
@@ -0,0 +1,26 @@
+var gpio = require("pi-gpio");
+
+var ledPin = 7;
+
+var ledOn = function(callback) {
+  gpio.open(ledPin, "output", function(err) {
+    gpio.write(ledPin, 1, function() {
+
+    });
+  });
+}
+
+var ledOff = function(callback) {
+  gpio.close(ledPin, callback);
+}
+
+var shutdown = function(){
+  ledOff(process.exit);
+};
+
+var repl = require('repl');
+
+var replServer = repl.start('> ');
+replServer.context.ledOn = ledOn;
+replServer.context.ledOff = ledOff;
+replServer.context.shutdown = shutdown;
